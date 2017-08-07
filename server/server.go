8a04a1f0a72e5f5546ad07016d56f42b96033e3c
A Arrow <oneone@gmail.com>
2017-08-07 02:39:58 +0000

    blah

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import "net"
 import "fmt"
 import "bufio"
+import "strings"
 
 func Start() {
 	s, err := net.Listen("tcp", ":3002")
@@ -35,12 +36,23 @@ func clientConns(listener net.Listener) chan net.Conn {
 
 func handleConn(client net.Conn) {
 	client.Write([]byte("WELCOME TO HDT, STATE YOUR VERSION\n"))
+	lastLine := ""
+	i := 0
 	b := bufio.NewReader(client)
 	for {
+		if i == 1 {
+			if lastLine == "1" {
+				client.Write([]byte("I THINK THE LONGEST CHAIN IS BLAH\n"))
+			} else {
+				client.Write([]byte("PLEASE UPGRADE\n"))
+				break
+			}
+		}
 		line, err := b.ReadBytes('\n')
 		if err != nil {
 			break
 		}
-		fmt.Println(line)
+		lastLine = strings.TrimSpace(string(line))
+		i++
 	}
 }
